4c4d215f564189b6b9638b0a2c89660b2c0ed0ee
起床啦！ <1106728884@qq.com>
2022-11-13 17:52:36 +0800

    项目搭建

---
+++ README.md
@@ -0,0 +1,62 @@
+# wangEditor（vue3版）
+***vue3版本*** | [**vue2版本**](https://github.com/QuietHear/vue-wangEditor-block '浏览')
+
+
+## 安装
+	npm i vue-wangEditor-block-plus
+
+## 使用
+	import vueWangeditorBlockPlus from "vue-wangeditor-block-plus";
+	import "vue-wangeditor-block-plus/index.css";
+	
+	app.use(vueWangeditorBlockPlus);
+
+
+## 1. 参数
+* `v-model`：当前文本内容-->String;非必传;默认*''*
+
+* `editorMode`：编辑器的展示模式-->String;非必传;默认*'default'*
+
+* `toolbarConfig`：工具栏配置-->Object;非必传
+
+* `editorConfig`：编辑器配置-->Object;非必传
+
+* `customPaste`：自定义粘贴事件，详见官方文档-->Function;非必传
+
+* `customAlert`：自定义alert事件，详见官方文档-->Function;非必传
+
+* `diyExtend`：自定义扩展，详见官方文档-->Object;非必传
+>
+	这个地方注册的新菜单等，需要自己在toolbarConfig,editorConfig等对应的配置中加入对应的key才能生效
+>
+
+* `language`：编辑器语言，详见官方文档-->String;非必传;默认*'zh-CN'*
+
+* `languageExtend`：扩展语言包，详见官方文档-->Array;非必传
+>
+	扩展语言包可以传入多个，格式如下
+	{
+		key:'语言的唯一值',
+		value:{详细内容}
+	}
+>
+
+* `onlyShow`：只展示内容-->Boolean;非必传;默认*false*
+>
+	为true时会将编辑的配置中默认禁用掉
+>
+
+
+* `@getRef`：初始化成功时触发，会抛出编辑器与工具栏的ref
+
+* `@change`：内容改变时触发，会抛出编辑器ref
+
+* `@destroyed`：编辑器销毁时触发，会抛出编辑器ref
+
+* `@focus`：编辑器foucs时触发，会抛出编辑器ref
+
+* `@blur`：编辑器blur时触发，会抛出编辑器ref
+
+
+## 2. 官方文档
+[http://www.wangeditor.com/doc/](http://www.wangeditor.com/doc/ '文档')
---
+++ lib/index.vue
@@ -0,0 +1,212 @@
+/*
+ * @Author: aFei
+ * @Date: 2022-11-10 14:56:59
+*/
+/*
+ * @LastEditors: aFei
+ * @LastEditTime: 2022-11-11 10:37:10
+*/
+<template>
+  <div :class="['vue-wangEditor-block-plus', onlyShow ? 'only-show' : '']">
+    <Toolbar
+      class="editor-toolbar"
+      :editor="editorRef"
+      :defaultConfig="toolbarConfig"
+      :mode="editorMode"
+      v-if="!toolbarHide"
+      v-show="!onlyShow"
+    />
+    <div class="editor-toolbar else" v-else></div>
+    <Editor
+      class="editor-content"
+      v-model="modelValue"
+      :defaultConfig="editorConfig"
+      :mode="editorMode"
+      @onCreated="editorCreated"
+      @onChange="editorChange"
+      @onDestroyed="editorDestroyed"
+      @onFocus="editorFocus"
+      @onBlur="editorBlur"
+      @customAlert="customAlert"
+      @customPaste="customPaste"
+      v-if="!toolbarHide"
+    />
+    <div class="editor-content" v-else></div>
+  </div>
+</template>
+<script>
+// 官网：https://www.wangeditor.com/v5/
+import "@wangeditor/editor/dist/css/style.css";
+import {
+  Boot,
+  DomEditor,
+  i18nAddResources,
+  i18nChangeLanguage,
+} from "@wangeditor/editor";
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+import { ref, nextTick, onBeforeUnmount, shallowRef, watch } from "vue";
+export default {
+  name: "vueWangeditorBlockPlus",
+  components: { Editor, Toolbar },
+  emits: [
+    "update:modelValue",
+    "getRef",
+    "change",
+    "destroyed",
+    "focus",
+    "blur",
+  ],
+  props: {
+    // 文本域内容
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    // 编辑器模式('default','simple')
+    editorMode: {
+      type: String,
+      default: "default",
+    },
+    // 工具栏配置
+    toolbarConfig: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    // 编辑器配置
+    editorConfig: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    // 自定义粘贴事件
+    customPaste: {
+      type: Function,
+      default: (editor, event, callback) => {
+        callback(true);
+      },
+    },
+    // 自定义alert事件
+    customAlert: {
+      type: Function,
+      default: (s, t) => {
+        alert(s);
+      },
+    },
+    // 自定义扩展
+    diyExtend: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    // 编辑器语言
+    language: {
+      type: String,
+      default: "zh-CN",
+    },
+    // 扩展语言包
+    languageExtend: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    // 只展示内容
+    onlyShow: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { attrs, slots, emit, expose }) {
+    // 注入自定义扩展
+    Boot.registerMenu(props.diyExtend);
+    // 注入新语言库
+    props.languageExtend.forEach((item) => {
+      i18nAddResources(item.key, item.value);
+    });
+    // 注销实例
+    const deleteEditor = () => {
+      if (editorRef.value) {
+        editorRef.value.destroy();
+        editorRef.value = null;
+      }
+    };
+    let toolbarHide = ref(false);
+    // 监听语言改变
+    watch(
+      () => props.language,
+      () => {
+        i18nChangeLanguage(props.language);
+        deleteEditor();
+        toolbarHide.value = true;
+        setTimeout(() => {
+          toolbarHide.value = false;
+        }, 0);
+      }
+    );
+    // 设置系统语言
+    i18nChangeLanguage(props.language);
+    // 改变只读模式
+    const checkShow = () => {
+      if (props.onlyShow) {
+        editorRef.value.disable();
+      } else {
+        editorRef.value.enable();
+      }
+    };
+    // 监听只读状态改变
+    watch(
+      () => props.onlyShow,
+      () => {
+        checkShow();
+      }
+    );
+    // 编辑器实例，必须用 shallowRef声明
+    const editorRef = shallowRef();
+    // 组件销毁时，也及时销毁编辑器
+    onBeforeUnmount(() => {
+      deleteEditor();
+    });
+    // 实例加载后触发
+    const editorCreated = (editor) => {
+      editorRef.value = editor;
+      nextTick(() => {
+        // 抛出编辑器实例与工具栏实例
+        emit("getRef", editor, DomEditor.getToolbar(editor));
+        checkShow();
+      });
+    };
+    // 实例内容变化时触发
+    const editorChange = (editor) => {
+      emit("update:modelValue", props.modelValue);
+    };
+    // 实例销毁后触发
+    const editorDestroyed = (editor) => {
+      emit("destroyed", editor);
+    };
+    // 实例focus时触发
+    const editorFocus = (editor) => {
+      emit("focus", editor);
+    };
+    // 实例blur时触发
+    const editorBlur = (editor) => {
+      emit("blur", editor);
+    };
+    return {
+      editorRef,
+      editorCreated,
+      editorChange,
+      editorDestroyed,
+      editorFocus,
+      editorBlur,
+      toolbarHide,
+    };
+  },
+};
+</script>
+<style lang="scss">
+@use "style/index.scss" as *;
+</style>
