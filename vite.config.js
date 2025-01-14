1e7c69f9c415b30f670a5430ab80b9b1fd86b2d0
yps <1106728884@qq.com>
2025-01-14 09:20:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@
 */
 /*
  * @LastEditors: aFei
- * @LastEditTime: 2023-09-21 15:16:09
+ * @LastEditTime: 2025-01-03 13:36:54
 */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
@@ -42,7 +42,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => ({
       },
     },
   },
-  plugins: [vue(), autoImport({ imports: ['vue', 'vue-router', { from: 'vue-router', imports: ['createRouter', 'createWebHistory'] }], dts: false })],
+  plugins: [vue(), autoImport({ imports: ['vue', { from: 'vue', imports: ['isVNode'] }, 'vue-router', { from: 'vue-router', imports: ['createRouter', 'createWebHistory'] }], dts: false })],
   css: {
     preprocessorOptions: {
       scss: {
